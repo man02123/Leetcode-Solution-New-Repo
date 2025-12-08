7c0e0b933197aec3e3ab0eebce0ee3ff8ce7a5e3
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:31:22 +0530

    Create 1700-MinimumTimeToMakeRopeColorful

---
+++ 1700-MinimumTimeToMakeRopeColorful/1700-MinimumTimeToMakeRopeColorful.cpp
@@ -0,0 +1,17 @@
+// Last updated: 08/12/2025, 17:31:20
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int cost=0;
+        for(int i=1;i<=colors.size();i++)
+        {
+            if(colors[i]==colors[i-1])
+            {
+                cost+=min(neededTime[i],neededTime[i-1]);
+               neededTime[i]=max(neededTime[i],neededTime[i-1])  ;
+            }
+           
+        }
+        return cost;
+    }
+};
