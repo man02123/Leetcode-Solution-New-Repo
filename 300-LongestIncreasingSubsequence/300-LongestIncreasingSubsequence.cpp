38653060933d93a0bedba17d7703a7b8a6f4db5a
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:37:51 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -0,0 +1,19 @@
+// Last updated: 08/12/2025, 17:37:48
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+  
+     vector<int> res{nums[0]};
+        for(int i=1;i<n;i++)
+        {
+         int ind=(lower_bound(res.begin(),res.end(),nums[i])-res.begin());
+            
+            if(ind>=res.size())
+                res.push_back(nums[i]);
+            else
+                res[ind]=nums[i];
+        }
+        return res.size();
+    }
+};
