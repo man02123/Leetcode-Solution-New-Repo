f499536c8ecb086b8c1940cc1e9a4ce456d7bbc2
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:33:32 +0530

    Create 1087-LongestArithmeticSubsequence

---
+++ 1087-LongestArithmeticSubsequence/1087-LongestArithmeticSubsequence.cpp
@@ -0,0 +1,28 @@
+// Last updated: 08/12/2025, 17:33:30
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n = nums.size();
+       vector<vector<int>> dp(nums.size(),vector<int>(2000,0));
+        int res= 1;
+        for(int i =1;i<nums.size();i++){
+            for(int j=0 ;j<i;j++){
+                
+                int diff = nums[i]-nums[j]+500;
+                
+                if(dp[j][diff]>0)
+                {
+                    dp[i][diff] =1+dp[j][diff];
+                }
+                else
+                    dp[i][diff] = 2;
+              res = max(res,dp[i][diff]) ;    
+            }
+            
+        }
+        return res;
+        
+        
+        
+    }
+};
