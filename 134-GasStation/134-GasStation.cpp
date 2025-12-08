c16bb8547918973711f0d25ef1d7c0ec47e69503
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:39:10 +0530

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:39:08
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& g, vector<int>& c) {
+        int sum=0;
+        int ind=0;
+        int totalsum=0;
+        for(int i=0;i<g.size();i++)
+        {
+            totalsum+=g[i]-c[i]; 
+            sum+=g[i]-c[i];
+            if(sum<0)
+            {
+                ind=i+1;
+                sum=0;
+            }
+            
+            
+        }
+        if(totalsum<0)return -1;
+        
+        return ind;
+    }
+};
