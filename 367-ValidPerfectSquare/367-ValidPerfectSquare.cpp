3dc87f3ad6e82103e1aa2952cc4fcbef805c80f0
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:37:30 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:37:28
+class Solution {
+public:
+    bool isPerfectSquare(int n) {
+         int l = 1,r = n;
+    int ans = n;
+    while (l <= r) {
+      int mid = l+(r-l)/2;
+
+      if ((n%mid) == 0 && (n/mid)==mid) {
+          return true;
+      } else if (n/mid < mid) {
+          
+          r = mid-1;
+      }
+      else{
+          ans = mid;
+          l = mid+1;
+      }
+    }
+    return false;
+        
+    }
+};
