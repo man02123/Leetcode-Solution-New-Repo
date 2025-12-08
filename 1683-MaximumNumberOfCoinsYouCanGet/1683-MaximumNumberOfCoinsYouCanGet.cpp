c0b04c22725e8609b3e129bd5484d80eb6b98a99
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:31:26 +0530

    Create 1683-MaximumNumberOfCoinsYouCanGet

---
+++ 1683-MaximumNumberOfCoinsYouCanGet/1683-MaximumNumberOfCoinsYouCanGet.cpp
@@ -0,0 +1,12 @@
+// Last updated: 08/12/2025, 17:31:24
+class Solution {
+public:
+    int maxCoins(vector<int>& p) {
+        sort(begin(p),end(p));
+        int sum = 0;
+        int cov = 0;
+        for(int i = p.size()-2;i>=0 && i>cov;i-=2)
+        sum += p[i],cov++;
+        return sum;
+    }
+};
