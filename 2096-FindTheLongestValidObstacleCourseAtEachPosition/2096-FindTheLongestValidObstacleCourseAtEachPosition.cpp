4e74d29e1615cd4cd9a382995fcfd514b3c84352
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:30:03 +0530

    Create 2096-FindTheLongestValidObstacleCourseAtEachPosition

---
+++ 2096-FindTheLongestValidObstacleCourseAtEachPosition/2096-FindTheLongestValidObstacleCourseAtEachPosition.cpp
@@ -0,0 +1,29 @@
+// Last updated: 08/12/2025, 17:30:00
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& o) {
+        vector<int> lis(o.size(),1);
+        
+        vector<int> temp;
+        
+        for(int i = 0;i<o.size();i++)
+        {
+            int ind = upper_bound(temp.begin(),temp.end(),o[i]) - temp.begin();
+           
+            if(ind==temp.size())
+            {
+                
+                    temp.push_back(o[i]);
+                    lis[i]=temp.size();
+            }
+            
+            else
+            {
+                temp[ind] = o[i];
+                lis[i]=ind+1;
+            }
+        }
+        return lis;
+        
+    }
+};
