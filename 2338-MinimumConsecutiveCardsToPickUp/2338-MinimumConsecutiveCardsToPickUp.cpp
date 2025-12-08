d3b4718c590728ec5f41266678646dd776a95f11
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:09 +0530

    Create 2338-MinimumConsecutiveCardsToPickUp

---
+++ 2338-MinimumConsecutiveCardsToPickUp/2338-MinimumConsecutiveCardsToPickUp.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/12/2025, 17:29:07
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& c) {
+        unordered_map<int,int> mp;
+        
+        int res=INT_MAX;
+        int j=0;
+        
+        for(int i=0;i<c.size();i++)
+        {
+           if(mp.find(c[i])!=mp.end())
+               res=min(res,i-mp[c[i]]+1);
+            
+            mp[c[i]]=i;
+        }
+        return (res==INT_MAX)?-1:res;
+        
+    }
+};
