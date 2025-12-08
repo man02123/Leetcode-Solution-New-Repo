fa21ff2f5dfa09cc6e607fb6be456d3763a51433
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:37:06 +0530

    Create 410-SplitArrayLargestSum

---
+++ 410-SplitArrayLargestSum/410-SplitArrayLargestSum.cpp
@@ -0,0 +1,57 @@
+// Last updated: 08/12/2025, 17:37:03
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l =0, r = accumulate(nums.begin(),nums.end(),0);
+        int ans = 0;
+        
+        while(l<r){
+            int mid = l+(r-l)/2;
+            
+            if(check(nums , k ,mid)){
+                ans = mid;
+                r = mid;
+            }
+            else
+                l = mid+1;
+        }
+        if(k==1)
+            return accumulate(nums.begin(),nums.end(),0);
+        
+        return ans;
+        
+    }
+    int check(vector<int>& nums  , int part , int mid){
+        
+        int currpart = 0;
+        int csum = 0;
+        int mx = 0;
+        
+        for(int i =0;i<nums.size();i++){
+            if(mid < nums[i])
+                return false;
+            
+            if(nums[i]+csum > mid){
+                currpart++;
+                csum = nums[i];
+                mx = max(csum , mx);
+            }
+            else{
+                csum += nums[i];
+                mx = max(csum , mx);
+            }
+        }
+        //cout<<mx<<mid<< " ";
+        currpart++;
+        
+        // if(mx > mid)
+        //     return false;
+        
+        if(currpart <= part)
+            return true;
+        
+        return false;
+        
+    }
+};
