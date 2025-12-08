3061d74ecefd69379e3527a9a6fb52ec7e9b25a5
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:40:03 +0530

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:40:01
+class Solution {
+public:vector<vector<int>> res;
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        recur(n,k,1,temp);
+        return res;
+    }
+    void recur(int n,int k,int ind,vector<int> &temp)
+    {
+        if(temp.size()==k)
+        {
+            res.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<=n;i++)
+        {
+            temp.push_back(i);
+            recur(n,k,i+1,temp);
+            temp.pop_back();
+            //recur(n,k,i+1,temp);
+        }
+    }
+};
