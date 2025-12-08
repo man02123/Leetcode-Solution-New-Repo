2b38e3c984a872b88988ea8f7a676fbdb8365545
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:50 +0530

    Create 2134-MaximizeTheConfusionOfAnExam

---
+++ 2134-MaximizeTheConfusionOfAnExam/2134-MaximizeTheConfusionOfAnExam.cpp
@@ -0,0 +1,26 @@
+// Last updated: 08/12/2025, 17:29:48
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        int res=0;
+        int fmax=0;
+        
+        unordered_map<char,int> mp;
+        int i=0;
+        
+        for(int j=0;j<s.size();j++)
+        {
+            mp[s[j]]++;
+            fmax=max(fmax,mp[s[j]]);
+            if(j-i+1-fmax<=k)
+            { res=max(res,j-i+1);
+                continue;
+            }
+            
+            mp[s[i++]]--;
+            
+        }
+        return res;
+        
+    }
+};
