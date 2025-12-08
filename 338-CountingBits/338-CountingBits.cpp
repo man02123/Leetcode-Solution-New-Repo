0158dcbd7145d559958d2e850669f400dc4e9e91
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:37:38 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,28 @@
+// Last updated: 08/12/2025, 17:37:35
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+       // res.push_back(0);
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        
+        for(int i = 0;i<=n ;i++){
+            res.push_back(solve(i,dp));
+        }
+        return res;
+    }
+    int solve(int num , int dp[]){
+        if(num==0)
+            return 0;
+        if(num == 1)
+            return 1;
+        if(dp[num] != -1)
+            return dp[num];
+        
+        if(num%2 == 0)
+            return dp[num] = solve(num/2,dp);
+        else
+            return dp[num] = 1+solve(num/2,dp);
+    }
+};
