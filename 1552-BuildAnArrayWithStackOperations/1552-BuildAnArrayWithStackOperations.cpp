a1a3a406a7abcb5b6b72d70edd1c7a367833af7f
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:32:01 +0530

    Create 1552-BuildAnArrayWithStackOperations

---
+++ 1552-BuildAnArrayWithStackOperations/1552-BuildAnArrayWithStackOperations.cpp
@@ -0,0 +1,31 @@
+// Last updated: 08/12/2025, 17:31:59
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& t, int n) {
+        
+        vector<string >ans;
+        int cptr = 0;
+        int prev =-1;
+        stack<int> st;
+        for(int i =1;i<=n;i++){
+            if(cptr < t.size() && t[cptr] == i){
+                while(st.size() >0 && st.top() < i && st.top()!=prev){
+                    st.pop();
+                    ans.push_back("Pop");
+                }
+                st.push(i);
+                ans.push_back("Push");
+                if(cptr < t.size())
+                prev = t[cptr];
+                
+                cptr++;
+                if(cptr == t.size())
+                    return ans;
+            }else{
+                st.push(i);
+                ans.push_back("Push");
+            }
+        }
+        return ans;
+    }
+};
