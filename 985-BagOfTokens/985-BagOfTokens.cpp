1365413551789887e90d3c2e5e3a0d52620133ad
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:34:01 +0530

    Create 985-BagOfTokens

---
+++ 985-BagOfTokens/985-BagOfTokens.cpp
@@ -0,0 +1,33 @@
+// Last updated: 08/12/2025, 17:33:59
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& t, int power) {
+        sort(t.begin(),t.end());
+        
+        int score=0;
+        int maxsc=0;
+        int fdown=t.size()-1;
+        int fup=0;
+        while(fup<=fdown)
+        {
+            if(power>=t[fup])
+            {
+                power-=t[fup];
+                score++;
+                maxsc=max(maxsc,score);
+                fup++;
+            }
+            else if(score>0)
+            {
+               score--;
+                power+=t[fdown];
+                fdown--;
+            }
+            else
+                break;
+            
+        }
+        return maxsc;
+        
+    }
+};
