06804e6efd70619d88ee444ab96e2d1972c1af7d
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:38:31 +0530

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -0,0 +1,35 @@
+// Last updated: 08/12/2025, 17:38:29
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pr) {
+        vector<int> g[n];
+        vector<int> indeg(n);
+        
+        for(auto it:pr){
+            g[it[1]].push_back(it[0]);
+            indeg[it[0]]++; 
+        }
+        queue<int> q;
+        for(auto i = 0;i<n;i++)
+            if(indeg[i] == 0)
+                q.push(i);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(auto it:g[node]){
+                indeg[it]--;
+                
+                if(indeg[it]==0)
+                    q.push(it);
+            }
+            
+        }
+        for(auto it:indeg)
+            if(it!=0)return false;
+        
+        return true;
+        
+    }
+};
