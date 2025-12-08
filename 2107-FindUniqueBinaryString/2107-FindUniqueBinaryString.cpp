a1748731f3a755a0a6c5a5a890b10726efc11fbc
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:58 +0530

    Create 2107-FindUniqueBinaryString

---
+++ 2107-FindUniqueBinaryString/2107-FindUniqueBinaryString.cpp
@@ -0,0 +1,28 @@
+// Last updated: 08/12/2025, 17:29:56
+class Solution {
+public:string res;
+    unordered_set<string> st;
+    string findDifferentBinaryString(vector<string>& nums) {
+        
+        for(auto it:nums)st.insert(it);
+        string temp;
+        permute(temp,nums.size());
+        return res;
+    }
+    void permute(string s ,int n)
+    {
+        if(s.size()>n)return;
+        
+        if(s.size()==n && st.find(s)==st.end())
+        {
+            res=s;
+            return;
+        }
+        string t=s;
+        t.push_back('0');
+        permute(t,n);
+       
+        s.push_back('1');
+        permute(s,n);
+    }
+};
