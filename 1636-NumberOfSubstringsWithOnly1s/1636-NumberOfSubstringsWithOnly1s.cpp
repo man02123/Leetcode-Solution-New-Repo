a4751ef97bf030c7c50df67eb137a6e1cfacd1f4
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:31:41 +0530

    Create 1636-NumberOfSubstringsWithOnly1s

---
+++ 1636-NumberOfSubstringsWithOnly1s/1636-NumberOfSubstringsWithOnly1s.cpp
@@ -0,0 +1,28 @@
+// Last updated: 08/12/2025, 17:31:38
+class Solution {
+public:int mod = 1000000000+7;
+    int numSub(string s) {
+        long long ans = 0;
+       
+        int len =1;
+        for(int r = 1;r<s.size();r++){
+            
+             if(s[r] == s[r-1] && s[r]=='1')
+             {
+                 len++;
+                 ans = (ans+len)%mod;
+             } 
+            else if(s[r] == '1')
+            len = 1,ans = (ans+len)%mod;
+            
+            
+           
+            
+            
+        }
+        if(s[0] == '1')
+            ans++;
+        
+        return ans ;
+    }
+};
