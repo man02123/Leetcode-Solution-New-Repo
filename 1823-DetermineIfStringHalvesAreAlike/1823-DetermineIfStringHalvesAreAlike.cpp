6e932f8d8cfc47af5a4b2a761f3039e32fc8b467
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:30:54 +0530

    Create 1823-DetermineIfStringHalvesAreAlike

---
+++ 1823-DetermineIfStringHalvesAreAlike/1823-DetermineIfStringHalvesAreAlike.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:30:52
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int i,t=0;
+       for(i=0;i<(s.size())/2;++i)
+       {
+           char ch=s[i];
+           if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'|| ch=='A' || ch=='E'|| ch=='I'|| ch=='O'|| ch=='U')
+               t++;
+       }
+         for(i=(s.size())/2;i<s.size();++i)
+       {
+           char ch=s[i];
+           if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'|| ch=='A' || ch=='E'|| ch=='I'|| ch=='O'|| ch=='U')
+               t--;
+       }
+       if(t==0)
+           return true;
+        
+        
+        return false;
+    }
+};
