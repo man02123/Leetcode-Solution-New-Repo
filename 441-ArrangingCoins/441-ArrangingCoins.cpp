4bce01160b24beca56580989d800a5bada72220a
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:36:57 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.cpp
@@ -0,0 +1,9 @@
+// Last updated: 08/12/2025, 17:36:55
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        int ans=(-0.5+sqrt((double)2*n+(0.25)));
+         return ans;        
+    }
+};
