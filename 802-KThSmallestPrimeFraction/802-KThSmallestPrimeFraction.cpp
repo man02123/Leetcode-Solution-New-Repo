52f5ba6d8a404dfd1120751261fc63107ab1315a
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:34:56 +0530

    Create 802-KThSmallestPrimeFraction

---
+++ 802-KThSmallestPrimeFraction/802-KThSmallestPrimeFraction.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/12/2025, 17:34:54
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        pair<double,pair<int,int>> p;
+        priority_queue<pair<double,pair<int,int>>> pq;
+        
+        for(int i=0;i<arr.size()-1;i++)
+            for(int j=i+1;j<arr.size();j++)
+            {
+                //cout<<arr[i]/arr[j]<<" ";
+                p={(double)arr[i]/arr[j],{arr[i],arr[j]}};
+                pq.push(p);
+                if(pq.size()>k)
+                    pq.pop();
+            }
+        
+     return {pq.top().second.first,pq.top().second.second};  
+    }
+};
