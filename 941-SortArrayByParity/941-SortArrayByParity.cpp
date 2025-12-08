da6c172c5af6091412c27c3fe848fe74731c22d4
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:34:21 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.cpp
@@ -0,0 +1,15 @@
+// Last updated: 08/12/2025, 17:34:19
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i=0;
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j]%2==0)
+                swap(nums[i++],nums[j]); 
+        }
+      
+        return nums;
+        
+    }
+};
