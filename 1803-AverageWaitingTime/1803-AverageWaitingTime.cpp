e83713751bc0fd9666991ba1ab427336163e6dce
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:30:59 +0530

    Create 1803-AverageWaitingTime

---
+++ 1803-AverageWaitingTime/1803-AverageWaitingTime.cpp
@@ -0,0 +1,31 @@
+// Last updated: 08/12/2025, 17:30:57
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& c) {
+        
+        int tcook = 0;
+        double wait = 0;
+        
+        for(int i = 0;i<c.size() ;i++)
+        {
+             int atime = c[i][0];
+            
+            tcook=max(tcook,c[i][0]);
+            
+            wait+=tcook+c[i][1]-atime;
+            
+            tcook+=c[i][1];
+            
+//             int dtime = c[i][1];
+            
+//             wait+=dtime - atime;
+        
+            
+   
+        }
+        double ans;
+        ans = wait/(c.size());
+        return ans;
+        
+    }
+};
