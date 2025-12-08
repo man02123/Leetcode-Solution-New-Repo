b377e355b35ddb6e97ab647616027780d4c13fa4
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:36:28 +0530

    Create 508-MostFrequentSubtreeSum

---
+++ 508-MostFrequentSubtreeSum/508-MostFrequentSubtreeSum.cpp
@@ -0,0 +1,39 @@
+// Last updated: 08/12/2025, 17:36:26
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:vector<int> res;
+    int maxf;
+    unordered_map<int,int> mp;
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        maxf=0;
+     int x= solve(root);
+     
+        bool flag=true;
+        for(auto it:mp)
+            if(it.second==maxf)
+                res.push_back(it.first);
+        
+        return res;
+    }
+    int solve(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+       int f=solve(root->left) + solve(root->right)+root->val;
+        mp[f]++;
+        maxf=max(maxf,mp[f]);
+        return f;   
+    }
+    
+    
+};
