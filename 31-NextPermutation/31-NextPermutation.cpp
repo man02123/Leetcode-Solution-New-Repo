ad2156038b97d294537d14927b51e6641b401a36
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:40:46 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,36 @@
+// Last updated: 08/12/2025, 17:40:44
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-1;
+        int flag = true;
+        
+        for(i  = n-2;i>=0;i--){
+            if(nums[i+1]>nums[i])
+            {
+                flag = false;
+             break;
+            }
+            
+        }
+        if(flag == true)
+        {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int ind = i;
+        
+        for(i=n-1;i>=0;i--)
+        {
+           if(nums[ind] < nums[i])
+           {
+               swap(nums[ind] , nums[i]);
+               break;
+           }
+            
+        }
+        reverse(nums.begin()+ind+1 , nums.end());
+        
+    }
+};
