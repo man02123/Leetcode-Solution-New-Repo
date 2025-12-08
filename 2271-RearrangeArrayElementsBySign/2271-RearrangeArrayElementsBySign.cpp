733d8123e4ff3d3c68b87d82a6b7bbca2eb661b3
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:19 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:29:17
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        int even = 0 ;
+        int odd = 1;
+
+        vector<int> ans(nums.size());
+
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] > 0){
+                ans[even] = nums[i] ;
+                even+=2 ;
+             }
+            else {
+                ans[odd] = nums[i] ; odd+=2 ;
+            }
+            
+        }
+
+        return ans;
+    }
+};
