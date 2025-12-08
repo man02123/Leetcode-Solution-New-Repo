e24768523a1118f3e28da5ec7745309c25d75975
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:39:18 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,15 @@
+// Last updated: 08/12/2025, 17:39:15
+class Solution {
+public:
+ bool isPalindrome(string s) {
+	int start=0, end=s.length()-1;
+	while(start<end) {
+		if (!isalnum(s[start])) start++;
+		else if (!isalnum(s[end])) end--;
+		else {
+			if (tolower(s[start++])!=tolower(s[end--])) return false;
+		}
+	}
+	return true;
+}
+};
