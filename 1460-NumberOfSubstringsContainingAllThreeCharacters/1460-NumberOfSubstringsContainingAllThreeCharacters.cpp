36635641aec989a2e86be47c162778f0ad2d54b9
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:32:20 +0530

    Create 1460-NumberOfSubstringsContainingAllThreeCharacters

---
+++ 1460-NumberOfSubstringsContainingAllThreeCharacters/1460-NumberOfSubstringsContainingAllThreeCharacters.cpp
@@ -0,0 +1,21 @@
+// Last updated: 08/12/2025, 17:32:18
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int a = -1 ,b = -1 , c= -1;
+        
+        int ans = 0 ;
+        
+        for(int i = 0; i<s.size() ;i++){
+            if(s[i] == 'a' )a=i;
+            else if(s[i] == 'b')b=i;
+            else
+                c=i;
+            
+            if(a!=-1 && b!=-1 && c!=-1)
+            ans += 1+min({a,b,c});
+            
+        }
+        return ans;
+    }
+};
