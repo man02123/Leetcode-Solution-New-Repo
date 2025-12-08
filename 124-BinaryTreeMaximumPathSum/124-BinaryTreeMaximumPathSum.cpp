c2cb1bc88dd1963ebcdcbce431b35b9d5d28e4ee
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:39:20 +0530

    Create 124-BinaryTreeMaximumPathSum

---
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:39:18
+class Solution {
+public:int maxi;
+    int maxPathSum(TreeNode* root) {
+        maxi=root->val;
+        int t= psum(root);
+        return maxi;
+    }
+    int psum(TreeNode* root)
+    {
+        if(root==NULL)
+            return -10001;
+        
+        int l= max(psum(root->left),0);
+        int r=max(psum(root->right),0);
+        
+        // int res=max({l,r,root->val});
+        
+        maxi=max({maxi,l+r+root->val,l+root->val,r+root->val});
+        
+        return max({root->val,l+root->val,r+root->val});
+        
+    }
+};
