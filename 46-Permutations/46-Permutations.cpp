188b95d97cfc1982fc21dc5a060d78ee17798fd3
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:40:33 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,26 @@
+// Last updated: 08/12/2025, 17:40:30
+class Solution {
+public:vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) {
+    vector<int> temp;
+        backtrack(nums,0);
+    return res;
+        
+    }
+    void backtrack(vector<int> &nums,int ind)
+    {
+        if(ind==nums.size()-1)
+        {
+            res.push_back(nums);
+            return;
+        }
+        
+        for(int i=ind;i<nums.size();i++)
+        {
+            
+            swap(nums[ind],nums[i]);
+            backtrack(nums,ind+1);
+            swap(nums[ind],nums[i]);
+        }
+    }
+};
