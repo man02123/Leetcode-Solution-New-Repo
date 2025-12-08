0633235daa591f2126da5f081cf7ff5bcc60a432
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:30:24 +0530

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.cpp
@@ -0,0 +1,30 @@
+// Last updated: 08/12/2025, 17:30:22
+class Solution {
+public:int cnt;
+    
+    int subsetXORSum(vector<int>& nums) {
+        cnt=0;
+        vector<int> temp;
+         gsub(nums,0,temp);
+        return cnt;
+    }
+    void gsub(vector<int>& nums,int ind,vector<int> &temp)
+    {
+        if(ind>nums.size())
+            return;
+        if(ind==nums.size())
+        {
+            int res=0;
+            for(auto it:temp)
+            {
+                res^=it;
+            }
+            cnt+=res;
+            return;
+        }
+        temp.push_back(nums[ind]);
+        gsub(nums,ind+1,temp);
+        temp.pop_back();
+        gsub(nums,ind+1,temp);
+    }
+};
