d175c34de94ce8b12507bc11f3bf45c307587b3c
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:28:44 +0530

    Create 2444-LongestIdealSubsequence

---
+++ 2444-LongestIdealSubsequence/2444-LongestIdealSubsequence.cpp
@@ -0,0 +1,28 @@
+// Last updated: 08/12/2025, 17:28:41
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        int lis[150]={};
+        
+        int res=0;
+        for(auto& i:s)
+        {
+            for(int j=i-k;j<=i+k;j++)
+                lis[i]=max(lis[i],lis[j]);
+                
+            res=max(res,++lis[i]);
+        }
+        return res;
+        
+    }
+};
+
+   // int longestIdealString(string s, int k) {
+   //      int dp[150] = {}, res = 0;
+   //      for (auto& i : s) {
+   //          for (int j = i - k; j <= i + k; ++j)
+   //              dp[i] = max(dp[i], dp[j]);
+   //          res = max(res, ++dp[i]);
+   //      }
+   //      return res;
+   //  }
