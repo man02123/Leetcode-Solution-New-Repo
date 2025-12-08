c7fabe32336dd9184f008de954b96c1c7bc123dd
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:35:38 +0530

    Create 674-LongestContinuousIncreasingSubsequence

---
+++ 674-LongestContinuousIncreasingSubsequence/674-LongestContinuousIncreasingSubsequence.cpp
@@ -0,0 +1,22 @@
+// Last updated: 08/12/2025, 17:35:35
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        if(size(nums)<=1)return 1;
+        
+        int res=1;
+        int len=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                len++;
+                res=max(res,len);
+            }
+            else
+                len=1;        
+        }
+        return res;
+        
+    }
+};
