ff80e3c8fc778d503bb51825350f8a1833ce4526
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:30:21 +0530

    Create 2016-ReductionOperationsToMakeTheArrayElementsEqual

---
+++ 2016-ReductionOperationsToMakeTheArrayElementsEqual/2016-ReductionOperationsToMakeTheArrayElementsEqual.cpp
@@ -0,0 +1,22 @@
+// Last updated: 08/12/2025, 17:30:19
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+       
+        
+        sort(nums.begin(),nums.end());
+        
+       int ans = 0;
+        int cfre = 0;
+        
+        for(int i=1;i<nums.size();i++){
+            
+            if(nums[i] != nums[i-1])cfre++;
+            
+            ans += cfre;
+        }
+            
+        
+        return ans;
+    }
+};
