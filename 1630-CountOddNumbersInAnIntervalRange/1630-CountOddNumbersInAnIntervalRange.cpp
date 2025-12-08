37621ca25ca92afe87e240acb80952d0cdd2a2fc
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:31:43 +0530

    Create 1630-CountOddNumbersInAnIntervalRange

---
+++ 1630-CountOddNumbersInAnIntervalRange/1630-CountOddNumbersInAnIntervalRange.cpp
@@ -0,0 +1,11 @@
+// Last updated: 08/12/2025, 17:31:41
+class Solution {
+public:
+      int countOdds(int l, int h) {
+        int res=0;
+        int range=h-l+1;
+        if(l%2==1 && h%2==1)
+            return range/2+1;
+        return range/2;
+    }
+};
