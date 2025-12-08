6529e9fab34077fa0144553fe0bf4ac09067c808
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:39:05 +0530

    Create 142-LinkedListCycleIi

---
+++ 142-LinkedListCycleIi/142-LinkedListCycleIi.cpp
@@ -0,0 +1,63 @@
+// Last updated: 08/12/2025, 17:39:02
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *h) {
+        if(h == NULL)
+            return NULL;
+        if(h->next!=nullptr && h->next == h)
+            return h;
+         if(h->next!=nullptr && h->next->next!=nullptr && h->next->next == h)
+            return h;
+     
+        ListNode *f = h,*s = h;
+        
+        while(f && f->next){
+            
+            f= f->next->next;
+            s = s->next;
+            if(f==s)
+            {
+                while(s!=h)
+                {
+                    s=s->next;
+                    h=h->next;
+                    if(h==s)
+                        return s;
+                }
+            }
+        }
+        return NULL;
+        
+//        ListNode *n = h;
+//         int cnt =0;
+//         while(n != s){
+//             n = n->next;
+//             cnt++;
+            
+//         }
+//         return cnt;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
