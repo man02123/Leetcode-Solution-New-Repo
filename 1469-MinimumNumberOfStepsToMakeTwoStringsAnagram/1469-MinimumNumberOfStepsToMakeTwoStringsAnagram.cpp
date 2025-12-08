aee8aacb00cea6cc96a0da4b64afd731a0576352
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:32:18 +0530

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/12/2025, 17:32:16
+class Solution {
+public:
+    int minSteps(string s, string t) {
+      
+        vector<int> v(26,0);
+        int res=0;
+        for(int i=0;i<s.size();i++)
+            v[s[i]-'a']++;
+        for(int i=0;i<t.size();i++)
+        {
+            if(v[t[i]-'a']>0)v[t[i]-'a']--;
+    
+        }
+       for(auto it:v)
+           if(it>0)res+=it;
+        
+        return res;
+    }
+};
