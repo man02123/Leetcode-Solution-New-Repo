53339f7cd2716b5d371c78730ffb9b1d8280f474
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:34:16 +0530

    Create 962-FlipStringToMonotoneIncreasing

---
+++ 962-FlipStringToMonotoneIncreasing/962-FlipStringToMonotoneIncreasing.cpp
@@ -0,0 +1,19 @@
+// Last updated: 08/12/2025, 17:34:13
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int pones=0;
+        int res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='1')
+                pones++;
+            else
+            { 
+              res=min(pones,res+1);
+            }
+        }
+        return res;
+        
+    }
+};
