f485dabc52b5c0d8aa35d236ff0fb9b8ed871165
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:38:07 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -0,0 +1,42 @@
+// Last updated: 08/12/2025, 17:38:04
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+      
+        deque<int> q;
+        int n = arr.size();
+        int j = 0;
+        vector<int> res;
+       
+        while(j<n && j<k)
+        {
+            while(q.size()>0 && arr[q.back()]<=arr[j])
+            q.pop_back();
+            
+            
+            q.push_back(j);
+            j++;
+        }
+        
+        res.push_back(arr[q.front()]);
+        
+        for(int i=j;i<n;i++)
+        {
+            while(q.size()>0 && q.front()<=i-k)
+            {
+                q.pop_front();
+            }
+            while(q.size()>0 && arr[q.back()]<arr[i])
+            q.pop_back();
+            
+            q.push_back(i);
+            
+            
+            res.push_back(arr[q.front()]);
+            
+            
+        }
+        return res;
+    }
+    
+};
