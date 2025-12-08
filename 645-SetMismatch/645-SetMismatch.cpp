00ecf793f82c118f6105411d958feb09eb5fce51
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:35:51 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -0,0 +1,24 @@
+// Last updated: 08/12/2025, 17:35:48
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+            mp[it]++;
+        vector<int> v;
+        for(auto it:mp)
+        {
+           if(it.second==2)
+               v.push_back(it.first);
+
+        }
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(mp[i]==0)
+                v.push_back(i);
+            continue;
+        }
+        return v;
+        
+    }
+};
