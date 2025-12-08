5c7157d2da345cf1d95830a02eadad6c5c38da09
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:39:53 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 08/12/2025, 17:39:50
+class Solution {
+public:vector<vector<int>> res;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> temp;
+        recur(nums,0,temp);
+        return res;
+    }
+    void recur(vector<int>& nums,int ind,vector<int>& temp)
+    {
+        res.push_back(temp);
+        
+        
+        for(int i=ind;i<nums.size();i++)
+        {
+            if(i>ind && nums[i]==nums[i-1])
+                continue;
+            temp.push_back(nums[i]);
+            recur(nums,i+1,temp);
+            temp.pop_back();
+            // recur(nums,i+1,temp);
+        }
+        
+    }
+};
