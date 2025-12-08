7c87d85a32b8c5a2dffe30aaec5c4c98c5c0423f
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:36:35 +0530

    Create 491-NonDecreasingSubsequences

---
+++ 491-NonDecreasingSubsequences/491-NonDecreasingSubsequences.cpp
@@ -0,0 +1,32 @@
+// Last updated: 08/12/2025, 17:36:33
+class Solution {
+public:set<vector<int>> mp;
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+     vector<int> v;
+        int ind=-1;
+        solve(nums,0,v);
+        
+        return vector<vector<int>>{mp.begin(),mp.end()};
+    }
+    void solve(vector<int>& nums,int ind,vector<int>&temp)
+    {
+        
+        if(ind>=nums.size())
+        {
+            if(temp.size()>1)
+                mp.insert(temp);
+            return;
+        }
+        
+       if(temp.size()==0 || temp.back()<=nums[ind])
+       {
+           temp.push_back(nums[ind]);
+           solve(nums,ind+1,temp);
+           temp.pop_back();
+           solve(nums,ind+1,temp);
+       }
+        else
+        solve(nums,ind+1,temp);
+   
+    }
+};
