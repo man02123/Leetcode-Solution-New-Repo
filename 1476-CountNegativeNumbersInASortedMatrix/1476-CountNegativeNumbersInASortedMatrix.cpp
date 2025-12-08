828904cf79350b4aadb8b6930d69656140e414de
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:32:17 +0530

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ 1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/12/2025, 17:32:15
+//class Solution {
+//public:
+//    int //countNegatives(vector<vector<int>>//& grid) {
+        class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count = 0;
+        // Iterate on all elements of the matrix one by one.
+        for (auto& row : grid) {
+            for (auto& element : row) {
+                // If the current element is negative, we count it.
+                if (element < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
