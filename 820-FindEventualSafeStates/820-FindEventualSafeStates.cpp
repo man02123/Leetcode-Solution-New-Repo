2158a75006903aa6e71e07f195f5fe5560951de3
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:34:46 +0530

    Create 820-FindEventualSafeStates

---
+++ 820-FindEventualSafeStates/820-FindEventualSafeStates.cpp
@@ -0,0 +1,41 @@
+// Last updated: 08/12/2025, 17:34:44
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& g) {
+       vector<int> outdegree(g.size(),0);
+        vector<vector<int>> newgraph(g.size());
+        
+        for(int i=0;i<g.size();i++)
+            for(int j=0;j<g[i].size();j++)
+                newgraph[g[i][j]].push_back(i),outdegree[i]++;;
+        
+        
+        
+        queue<int> q;
+        
+        for(int i=0;i<outdegree.size();i++)
+        {
+            if(outdegree[i]==0)
+                q.push(i);
+        }
+        
+        vector<int> res;
+       while(q.size()>0)
+       {
+           int node=q.front();
+           //cout<<node;
+           q.pop();
+           res.push_back(node);
+           for(auto it:newgraph[node])
+           {
+               outdegree[it]--;
+               if(outdegree[it]==0)
+                   q.push(it);
+           }
+               
+       }
+        sort(res.begin(),res.end());
+        return res;
+        
+    }
+};
