f8d5a5a14b721b7c6e7d26490c869c7ccc7136e0
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:33:55 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,19 @@
+// Last updated: 08/12/2025, 17:33:52
+class Solution {
+public:
+    int fib(int n) {
+        if(n<2)
+            return n;
+        int res=0;
+        
+        int lst=1;
+        int seclst=0;
+        for(int i=2;i<=n;i++)
+        {
+            res=lst+seclst;
+            seclst=lst;
+            lst=res;
+        }
+        return res;
+    }
+};
