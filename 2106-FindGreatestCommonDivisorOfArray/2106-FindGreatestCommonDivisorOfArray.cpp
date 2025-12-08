9e2c8c6c3fad930b31b3e2291ab02f5660e13d4f
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:59 +0530

    Create 2106-FindGreatestCommonDivisorOfArray

---
+++ 2106-FindGreatestCommonDivisorOfArray/2106-FindGreatestCommonDivisorOfArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 08/12/2025, 17:29:57
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mx=0,mn=INT_MAX;
+        for(auto it:nums)
+        {
+            mn=min(it,mn);
+            mx=max(it,mx);
+        }
+        for(int i=mn;i>=2;i--)
+            if(mx%i==0 && mn%i==0)return i;
+        
+        return 1;
+    }
+};
