d0e34ddb5ae2c8351ebfc21e2a8a3e30f1a0b711
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:29:28 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.cpp
@@ -0,0 +1,64 @@
+// Last updated: 08/12/2025, 17:29:25
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode *mid =head,*s = head,*prev = NULL;
+        
+        while(mid && mid->next)
+        {
+            mid=mid->next->next;
+            prev = s;
+            s = s->next;
+            
+            
+        }
+        prev ->next =NULL;
+        int maxi= 0;
+        
+        ListNode *y  = s;
+        ListNode *x = reverse(s);
+        // x->next = y;
+        // while(x)
+        // {
+        //     cout<<x->val;
+        //     x = x->next;
+        // }
+        
+        while(head!=NULL)
+        {
+            int temp = head->val;
+            temp+=x->val;
+            maxi = max(maxi,temp);
+            
+            head = head->next;
+            x = x->next;
+        }
+    
+       return maxi; 
+        
+    }
+    ListNode* reverse(ListNode* head)
+    {
+         ListNode* f,*prev = NULL;
+        
+        while(head!=NULL)
+        {
+            f = head->next;
+            head->next = prev;
+            prev = head;
+            
+            head = f;
+        }
+        return prev;
+    }
+};
