8251819cc1326ee653ecb9ecb22c019fdfec0c1d
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:32:21 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.cpp
@@ -0,0 +1,16 @@
+// Last updated: 08/12/2025, 17:32:19
+class Solution {
+public:
+    static bool compare(int a, int b) {
+        if (__builtin_popcount(a) == __builtin_popcount(b)) {
+            return a < b;
+        }
+        
+        return __builtin_popcount(a) < __builtin_popcount(b);
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
