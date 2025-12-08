f70c93e6ac5203d875086cbfbee553a4bf4c0c4e
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-08 17:40:29 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,22 @@
+// Last updated: 08/12/2025, 17:40:27
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(int it=0;it<strs.size();++it)
+        {
+            string x=strs[it];
+         sort(x.begin(),x.end());
+            
+             mp[x].push_back(strs[it]);
+        
+        
+        }
+        vector<vector<string>> s;
+    for(auto it=mp.begin();it!=mp.end();++it)
+    {
+        s.push_back(it->second);
+    }
+        return s;
+    }
+};
