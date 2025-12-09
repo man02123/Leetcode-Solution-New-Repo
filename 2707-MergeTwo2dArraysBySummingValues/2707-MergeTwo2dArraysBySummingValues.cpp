a806e083da5ea20a1cd6a0f7c94823da62716e92
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:43 +0530

    Create 2707-MergeTwo2dArraysBySummingValues

---
+++ 2707-MergeTwo2dArraysBySummingValues/2707-MergeTwo2dArraysBySummingValues.cpp
@@ -0,0 +1,31 @@
+// Last updated: 09/12/2025, 23:23:43
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& n1, vector<vector<int>>& n2) {
+       map<int,int> mp;
+        
+        for(auto it:n1)  
+            mp[it[0]]=it[1];
+        
+         for(auto it:n2)
+         {
+             if(mp.find(it[0])!=mp.end())
+             mp[it[0]]+=it[1];
+            else
+            mp[it[0]]=it[1];
+             
+         }
+            
+        
+        vector<vector<int>> res;
+        for(auto it:mp)
+        {
+            vector<int> temp;
+            temp.push_back(it.first);
+            temp.push_back(it.second);
+            res.push_back(temp);
+        }
+        return res;
+        
+    }
+};
