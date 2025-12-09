81e7575ae2a43714aa0a00bedd954292e7c301f7
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:14 +0530

    Create 2785-SemiOrderedPermutation

---
+++ 2785-SemiOrderedPermutation/2785-SemiOrderedPermutation.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:23:14
+class Solution {
+public:
+    int semiOrderedPermutation(vector<int>& nums) {
+        int lar=-1,s=-1;
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i]==nums.size())
+                lar=i;
+            
+            if(nums[i]==1)
+                s=i;   
+        }
+        
+        int dist= s+((nums.size()-1)-lar);
+        
+        if(lar<s)
+            dist--;
+        return dist;
+        
+    }
+};
