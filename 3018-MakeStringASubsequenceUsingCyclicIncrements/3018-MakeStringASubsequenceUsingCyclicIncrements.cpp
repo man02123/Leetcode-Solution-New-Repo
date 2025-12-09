549e074a2ac88db54182155da137819d3f8c7fc7
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:36 +0530

    Create 3018-MakeStringASubsequenceUsingCyclicIncrements

---
+++ 3018-MakeStringASubsequenceUsingCyclicIncrements/3018-MakeStringASubsequenceUsingCyclicIncrements.cpp
@@ -0,0 +1,24 @@
+// Last updated: 09/12/2025, 23:22:35
+class Solution {
+public:
+    bool canMakeSubsequence(string s1, string s2) {
+        
+        int r = 0 , l =0;
+        
+        while(l<s1.size() && r<s2.size()){
+            
+            int  myChar = s2[r]-'a';
+            
+            if( ((s1[l]-'a'+1)%26 == myChar) || ((s1[l]-'a')%26 == myChar) || ((s1[l]-'a')%26 == myChar))
+            {
+                r++;
+                l++;
+            }
+            else l++;
+        }
+        //cout<<r;
+        if(r==s2.size())return true;
+        
+     return false;   
+    }
+};
