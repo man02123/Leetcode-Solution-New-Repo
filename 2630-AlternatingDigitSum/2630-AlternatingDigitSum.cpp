28c5d7bfb7fe9149d2c01e606d67435165c05b08
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:24:11 +0530

    Create 2630-AlternatingDigitSum

---
+++ 2630-AlternatingDigitSum/2630-AlternatingDigitSum.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:24:11
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        string temp=to_string(n);
+        int res=0;
+       // bool flag=false;
+        
+        for(int i=0;i<temp.size();i++)
+        {
+            if((i)%2==0)
+            {
+                res+=(temp[i]-'0');
+            }
+            else
+            {
+                res-=(temp[i]-'0');
+            }
+        }
+        return res;
+        
+    }
+};
