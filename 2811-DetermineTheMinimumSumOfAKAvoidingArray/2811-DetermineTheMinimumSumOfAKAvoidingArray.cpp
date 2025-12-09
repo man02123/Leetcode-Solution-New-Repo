212b96d6dc3f92adc786fa97bc205fc257d86110
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:03 +0530

    Create 2811-DetermineTheMinimumSumOfAKAvoidingArray

---
+++ 2811-DetermineTheMinimumSumOfAKAvoidingArray/2811-DetermineTheMinimumSumOfAKAvoidingArray.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:23:03
+class Solution {
+public:
+    int minimumSum(int n, int k) {
+        unordered_map<int,int> mp;
+        
+        int num = 1;
+        int sum = 0;
+        
+        while(n>0){
+          
+            if(mp.find(k-num) == mp.end())
+            { 
+                sum += num;
+                mp[num]++;
+                n--;
+            }
+            num++;
+        }
+        return sum;
+        
+    }
+};
