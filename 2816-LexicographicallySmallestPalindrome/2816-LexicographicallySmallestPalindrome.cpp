ebe4e4506d76003af44a8fbc594516dad6bf76e6
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:02 +0530

    Create 2816-LexicographicallySmallestPalindrome

---
+++ 2816-LexicographicallySmallestPalindrome/2816-LexicographicallySmallestPalindrome.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:23:02
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        
+        int i = 0,r = s.size()-1;
+        while(i<r)
+        {
+            if(s[i]!=s[r])
+            {
+                if(s[i]>s[r])
+                    s[i] = s[r];
+                else
+                    s[r] = s[i];
+            }
+            i++;
+            r--;
+     
+        }
+        
+       return s; 
+    }
+};
