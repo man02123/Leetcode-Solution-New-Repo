55bbe5911218e56160c7f7a3b2e9cd16558d65d9
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:13 +0530

    Create 2786-FindTheLongestSemiRepetitiveSubstring

---
+++ 2786-FindTheLongestSemiRepetitiveSubstring/2786-FindTheLongestSemiRepetitiveSubstring.cpp
@@ -0,0 +1,32 @@
+// Last updated: 09/12/2025, 23:23:13
+class Solution {
+public:
+    int longestSemiRepetitiveSubstring(string s) {
+       // unordered_map<string,int> mp;
+        int ans = 1;
+        
+        for(int i = 0;i<s.size();i++)
+        {
+            
+            for(int j = 0;j<=i;j++){
+                int t =1;
+                for(int k =j+1;k<=i;k++)
+                {
+                   if(s[k] == s[k-1])
+                       t--;
+                    if(t>=0)
+                    {
+                        ans = max(ans,k-j+1);
+                    }
+                }
+               
+            }
+        }
+        
+        
+        return ans;
+        
+    }
+};
+//"0"
+   // ans =1
