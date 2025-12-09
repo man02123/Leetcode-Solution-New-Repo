6fa23fc359570cd04fbe74370467631d1d4dbb4e
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:33 +0530

    Create 3094-MinimumNumberOfOperationsToMakeArrayEmpty

---
+++ 3094-MinimumNumberOfOperationsToMakeArrayEmpty/3094-MinimumNumberOfOperationsToMakeArrayEmpty.cpp
@@ -0,0 +1,30 @@
+// Last updated: 09/12/2025, 23:22:33
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+            mp[it]++;
+        
+        int ans = 0;
+        
+        for(auto it:mp){
+            
+             int t = it.second;
+            if(t == 1)
+                return -1;
+           
+              
+             if(t%3 == 0)ans += t/3;
+             else {
+                 ans+= (t/3)+1;
+
+                 
+             }
+            
+          }
+            
+        return ans;
+        
+    }
+};
