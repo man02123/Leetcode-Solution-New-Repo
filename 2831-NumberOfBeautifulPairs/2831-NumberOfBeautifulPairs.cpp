6be657297699a6c46a55da359e9e80b73ba84c4b
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:56 +0530

    Create 2831-NumberOfBeautifulPairs

---
+++ 2831-NumberOfBeautifulPairs/2831-NumberOfBeautifulPairs.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:22:56
+class Solution {
+public:
+    int countBeautifulPairs(vector<int>& nums) {
+        int ans12 =0;
+        
+        for(int i =0;i<nums.size();i++){
+            
+            for(int j =i+1;j<nums.size();j++)
+            {
+                string y =to_string(nums[i]);
+                string x = to_string(nums[j]);
+                int lst = x.back()-'0';
+                int fst = y[0]-'0';
+            
+                
+                if(__gcd(lst,fst)==1)ans12++;
+            }
+        }
+        return ans12;
+        
+    }
+};
