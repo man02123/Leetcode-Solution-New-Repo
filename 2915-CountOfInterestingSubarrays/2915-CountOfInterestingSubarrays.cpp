bbed9474bc53d7abc3fc1262993a79d2198d56c8
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:41 +0530

    Create 2915-CountOfInterestingSubarrays

---
+++ 2915-CountOfInterestingSubarrays/2915-CountOfInterestingSubarrays.cpp
@@ -0,0 +1,30 @@
+// Last updated: 09/12/2025, 23:22:40
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int mod, int k) {
+        
+        long long ans = 0;
+        
+        long long csum = 0;
+        unordered_map<int,int> mp;
+        mp[0]++;
+        
+        for(int i = 0;i<nums.size();i++){
+           
+            if(nums[i]%mod == k)
+                csum++;
+            
+            long long temp = (csum-k) %mod; // if(csum-k) %mod is present => simplifing the equation cnt%mod == k to (csum-k)%mod
+            
+            if(mp.find(temp) != mp.end()){
+                ans += mp[temp];
+            }
+            
+            mp[csum%mod]++;  // as we need to work on remainder only so we store remainder only
+               
+        }
+        
+        
+        return ans;
+    }
+};
