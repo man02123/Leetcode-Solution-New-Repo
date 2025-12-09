ec342be377ee10eefcd15650e9214a399af232c2
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:25 +0530

    Create 2756-BuyTwoChocolates

---
+++ 2756-BuyTwoChocolates/2756-BuyTwoChocolates.cpp
@@ -0,0 +1,29 @@
+// Last updated: 09/12/2025, 23:23:24
+class Solution {
+public:
+    int buyChoco(vector<int>& p, int m) {
+        sort (p.begin(),p.end());
+        int mleft = m;
+            int cnd = 0;
+        
+            for(int i=0;i<p.size();i++)  
+            {
+                if(p[i]<=mleft)
+                {
+                    cnd++;
+                    mleft-=p[i];
+                }
+                
+                if(cnd==2)
+                    break;
+                
+            }
+        if(cnd ==2)
+            return mleft;
+        
+        
+        return m;
+        
+        
+    }
+};
