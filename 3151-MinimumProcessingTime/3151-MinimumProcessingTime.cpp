0ea73f58ef6579dfdbfdecd605817bf509e8aab9
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:26 +0530

    Create 3151-MinimumProcessingTime

---
+++ 3151-MinimumProcessingTime/3151-MinimumProcessingTime.cpp
@@ -0,0 +1,24 @@
+// Last updated: 09/12/2025, 23:22:26
+class Solution {
+public:
+    int minProcessingTime(vector<int>& p, vector<int>& t) {
+        vector<int> ntask;
+        
+        sort(t.begin() , t.end());
+        sort(p.begin(),p.end());
+        
+        for(int i =3;i<t.size();i+=4){
+            ntask.push_back(t[i]);   
+        }
+        reverse(ntask.begin(),ntask.end());
+        int ans = 0;
+        
+        for(int i =0;i<p.size();i++){
+            ans = max(ans,p[i]+ntask[i]);
+            
+        }
+        
+        return ans;
+        
+    }
+};
