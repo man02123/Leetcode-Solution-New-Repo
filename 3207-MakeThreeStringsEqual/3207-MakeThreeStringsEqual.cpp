b2a5cfcec04f564e69bad06ab5114c7d5aebff36
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:04 +0530

    Create 3207-MakeThreeStringsEqual

---
+++ 3207-MakeThreeStringsEqual/3207-MakeThreeStringsEqual.cpp
@@ -0,0 +1,19 @@
+// Last updated: 09/12/2025, 23:22:04
+class Solution {
+public:
+    int findMinimumOperations(string s1, string s2, string s3) {
+        if(s1==s2 && s2==s3)return 0;
+        
+        int ans = 0;
+        for(int i =0;i<min({s1.size(),s2.size(),s3.size()});i++){
+            if(s1[i] == s2[i] && s2[i]==s3[i]){
+                ans++;
+            }
+            else
+                break;
+        }
+        if(ans <1)return -1;
+        
+        return (s1.size()+s2.size()+s3.size())-(ans*3);
+    }
+};
