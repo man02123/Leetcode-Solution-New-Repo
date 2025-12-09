5bc9a89e3e9e071a826d226a119b96ac2d9fe790
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:40 +0530

    Create 2713-FindTheDivisibilityArrayOfAString

---
+++ 2713-FindTheDivisibilityArrayOfAString/2713-FindTheDivisibilityArrayOfAString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 09/12/2025, 23:23:39
+class Solution {
+public:
+    vector<int> divisibilityArray(string w, int m) {
+        long long currrem=0;
+        vector<int> res;
+        for(int i=0;i<w.size();i++)
+        {
+            currrem=currrem*10+(w[i]-'0');
+            if(currrem%m==0)
+            {
+                res.push_back(1);
+                currrem=0;
+            }
+            else
+            {
+                res.push_back(0);
+                currrem=currrem%m;
+            }
+        }
+        return res;
+    }
+};
