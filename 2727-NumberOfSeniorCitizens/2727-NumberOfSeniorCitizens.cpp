34d816b32a109333235c8f5faa2a21688f7a9963
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:35 +0530

    Create 2727-NumberOfSeniorCitizens

---
+++ 2727-NumberOfSeniorCitizens/2727-NumberOfSeniorCitizens.cpp
@@ -0,0 +1,19 @@
+// Last updated: 09/12/2025, 23:23:34
+class Solution {
+public:
+    int countSeniors(vector<string>& d) {
+        string temp;
+        int cnt = 0;
+        
+        for(auto it:d)
+        {
+            string s =it;
+            int x =s[11]-'0';
+            int y = s[12]-'0';
+            if(10*x+y>60)
+                cnt++;
+        }
+        return cnt;
+        
+    }
+};
