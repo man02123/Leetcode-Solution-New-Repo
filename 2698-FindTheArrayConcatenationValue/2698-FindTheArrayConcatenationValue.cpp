fb6572d8e0d10b51dc3f42c1ea5659db0bd201a2
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:23:45 +0530

    Create 2698-FindTheArrayConcatenationValue

---
+++ 2698-FindTheArrayConcatenationValue/2698-FindTheArrayConcatenationValue.cpp
@@ -0,0 +1,27 @@
+// Last updated: 09/12/2025, 23:23:45
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        int i=0,j=nums.size()-1;
+        
+       long long res=0;
+        while(i<=j)
+        {
+            string temp;
+            
+            if(i!=j)
+            {
+                temp+=to_string(nums[i++])+to_string(nums[j--]);
+                
+            }
+            else
+            {
+                temp+=to_string(nums[i++]);
+            }
+            res+=stol(temp);   
+        }
+        return res;
+        
+        
+    }
+};
