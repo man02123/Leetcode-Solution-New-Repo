d1afe29a0d9b2bd41e400eac6b09377134f52c7a
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:21:07 +0530

    Create 3398-MakeASquareWithTheSameColor

---
+++ 3398-MakeASquareWithTheSameColor/3398-MakeASquareWithTheSameColor.cpp
@@ -0,0 +1,32 @@
+// Last updated: 09/12/2025, 23:21:07
+class Solution {
+public:
+    bool canMakeSquare(vector<vector<char>>& g) {
+        
+        int res = 0;
+        
+         for(int i =1;i<=2;i++){
+              
+             for(int j =1 ;j<=2;j++){
+                 int b =0, w =0;
+                 if(g[i][j] == 'B')b++;
+                 else w++;
+                 
+                 if(g[i-1][j-1] == 'B')b++;
+                 else w++;
+                 
+                 if(g[i][j-1] == 'B')b++;
+                 else w++;
+                 
+                 if(g[i-1][j] == 'B')b++;
+                 else w++;
+                 
+                 if(max(b,w) >=3){
+                 res =1;
+             }
+                 
+             }
+             
+         }
+        return res;   }
+};
