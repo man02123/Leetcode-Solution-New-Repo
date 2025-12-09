7f05575a478a41b5421b6bd923e0f4496e022991
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:21:31 +0530

    Create 3279-AliceAndBobPlayingFlowerGame

---
+++ 3279-AliceAndBobPlayingFlowerGame/3279-AliceAndBobPlayingFlowerGame.cpp
@@ -0,0 +1,8 @@
+// Last updated: 09/12/2025, 23:21:30
+class Solution {
+public:
+    long long flowerGame(long long int n, long long int m) {
+       // long long int res = (long long int )(m*n/2);
+        return (m*n)/2;
+    }
+};
