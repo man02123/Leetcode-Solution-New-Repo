492e61eaeb9c31571ca7d0691cea3522f2cc3719
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:22 +0530

    Create 3165-FindIndicesWithIndexAndValueDifferenceI

---
+++ 3165-FindIndicesWithIndexAndValueDifferenceI/3165-FindIndicesWithIndexAndValueDifferenceI.cpp
@@ -0,0 +1,14 @@
+// Last updated: 09/12/2025, 23:22:22
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int in, int val) {
+        
+        for(int i =0 ;i<nums.size();i++){
+          for(int j = 0;j<=i;j++){
+              if(abs(i-j) >= in  && abs(nums[i]-nums[j]) >= val)
+                  return {i,j};
+          }
+        }
+        return {-1,-1};
+    }
+};
