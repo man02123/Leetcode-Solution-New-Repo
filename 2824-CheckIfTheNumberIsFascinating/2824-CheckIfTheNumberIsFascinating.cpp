fb6a8876d39ab317594063d8eeafb2cf7dfe3380
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:22:59 +0530

    Create 2824-CheckIfTheNumberIsFascinating

---
+++ 2824-CheckIfTheNumberIsFascinating/2824-CheckIfTheNumberIsFascinating.cpp
@@ -0,0 +1,24 @@
+// Last updated: 09/12/2025, 23:22:58
+class Solution {
+public:
+    bool isFascinating(int n) {
+        long long  x =2*n;
+        long long y = 3*n;
+        string a = to_string(x);
+        string b = to_string(y);
+        string c = to_string(n);
+        
+        string fin = c+a+b;
+        
+        sort(fin.begin(),fin.end());
+        
+        for(int i = 0;i<fin.size();i++)
+        {
+            if(fin[i]-'0' !=i+1)
+                return false;
+        }
+        return true;
+        
+        
+    }
+};
