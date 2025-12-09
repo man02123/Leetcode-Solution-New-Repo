cadb45d703422c522482adbf5c66370934be2828
Maneesh Kumar <118069900+man02123@users.noreply.github.com>
2025-12-09 23:24:00 +0530

    Create 2655-RearrangeArrayToMaximizePrefixScore

---
+++ 2655-RearrangeArrayToMaximizePrefixScore/2655-RearrangeArrayToMaximizePrefixScore.cpp
@@ -0,0 +1,26 @@
+// Last updated: 09/12/2025, 23:24:00
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        sort(nums.rbegin(),nums.rend());
+        int cnt=0;
+        int i=1;
+      vector<long long> arr;
+        long long psum=nums[0];
+       arr.push_back(psum);
+        if(psum>0)
+            cnt++;
+        
+        for(;i<nums.size();i++)
+        {
+            psum+=nums[i];
+            if(psum>0)
+                cnt++;
+            
+           arr.push_back(psum);
+        }
+       
+        return cnt;
+        
+    }
+};
